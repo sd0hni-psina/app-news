d58b23dc9e1df1ffa20ac375a2785027d92899b8
arsan <arsan_01@protonmail.com>
2025-09-03 14:13:34 +0600

    add in views.py, a view class RegisterView

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import status, generics, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import login
 
+from .models import User
+from .serializers import (
+    UserLoginSerializer,
+    UserProfileSerializer,
+    UserRegistrationSerializer,
+    UserUpdateSerializer,
+    ChangePasswordSerializer
+)
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.sace()
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'user': UserProfileSerializer(user).data,
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+            'message': 'User registered successfully'
+        }, status=status.HTTP_201_CREATED)
